9c8824c73394bf4d87ae6af7953d4ca30974b388
haphaak <carlosdirceu519@gmail.com>
2024-11-13 18:17:00 -0300

    .....

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const addProductForm = document.querySelector('#add-product-form');
 const updateProductForm = document.querySelector('#update-product-form');
 const updateProductId = document.querySelector('#update-id');
 const updateProductName = document.querySelector('#update-name');
+const updateProductDescription = document.querySelector('#update-description')
 const updateProductPrice = document.querySelector('#update-price');
+
 
 // Function to fetch all products from the server
 async function fetchProducts() {
-  const response = await fetch('http://18.191.230.254:3000/products');
+  const response = await fetch('http://3.17.56.192:3000/products');
   const products = await response.json();
 
   // Clear product list
@@ -16,7 +18,7 @@ async function fetchProducts() {
   // Add each product to the list
   products.forEach(product => {
     const li = document.createElement('li');
-    li.innerHTML = `${product.name} - $${product.price}`;
+    li.innerHTML = `${product.name} - ${product.description} - $${product.price}`;
 
     // Add delete button for each product
     const deleteButton = document.createElement('button');
@@ -33,6 +35,7 @@ async function fetchProducts() {
     updateButton.addEventListener('click', () => {
       updateProductId.value = product.id;
       updateProductName.value = product.name;
+      updateProductDescription.value = product.description;
       updateProductPrice.value = product.price;
     });
     li.appendChild(updateButton);
@@ -46,32 +49,44 @@ async function fetchProducts() {
 addProductForm.addEventListener('submit', async event => {
   event.preventDefault();
   const name = addProductForm.elements['name'].value;
+  const description = addProductForm.elements['description'].value;
   const price = addProductForm.elements['price'].value;
-  await addProduct(name, price);
+  await addProduct(name, description, price);
   addProductForm.reset();
   await fetchProducts();
 });
 
 // Function to add a new product
-async function addProduct(name, price) {
-  const response = await fetch('http://18.191.230.254:3000/products', {
+async function addProduct(name, description, price) {
+  const response = await fetch('http://3.17.56.192:3000/products', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ name, price })
+    body: JSON.stringify({ name, description, price })
   });
   return response.json();
 }
 
 // Function to delete a new product
 async function deleteProduct(id) {
-  const response = await fetch('http://18.191.230.254:3000/products/' + id, {
+  const response = await fetch('http://3.17.56.192:3000/products/' + id, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json'
     },
     //body: JSON.stringify({id})
+  });
+  return response.json();
+}
+
+async function updateProduct(name, description, price) {
+  const response = await fetch('http://3.17.56.192:3000/products', + name , description , price, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ name, description, price })
   });
   return response.json();
 }
